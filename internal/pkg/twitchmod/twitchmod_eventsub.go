dd6e278ed4f2d932a906fd9a95d2269cef24011b
m1stborn <kevin1997324@gmail.com>
2021-08-13 03:26:55 +0800

    Fix: notify message url

--- internal/pkg/twitchmod/twitchmod_eventsub.go
+++ internal/pkg/twitchmod/twitchmod_eventsub.go
@@ -65,10 +65,10 @@ func EventSubFollow(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte("ok"))
 
-	line.SendNotify(testAccessToken,
-		fmt.Sprintf("%s follows %s!",
-			followEvent.UserName,
-			followEvent.BroadcasterUserName))
+	//line.SendNotify(testAccessToken,
+	//	fmt.Sprintf("%s follows %s!",
+	//		followEvent.UserName,
+	//		followEvent.BroadcasterUserName))
 
 }
 
@@ -119,5 +119,5 @@ func EventSubStreamOnline(w http.ResponseWriter, r *http.Request) {
 	line.SendNotify(testAccessToken,
 		fmt.Sprintf("%s start streaming!\n https://www.twitch.tv/%s",
 			streamOnlineEvent.BroadcasterUserName,
-			streamOnlineEvent.BroadcasterUserID))
+			streamOnlineEvent.BroadcasterUserName))
 }
