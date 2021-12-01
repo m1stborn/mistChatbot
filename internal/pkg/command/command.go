953d66afe2986cbeefd1406ddcbac7803e0083a9
m1stborn <r10922178@ntu.edu.tw>
2021-12-01 17:02:59 +0800

    Feat: add chatbot command for YouTube

--- internal/pkg/command/command.go
+++ internal/pkg/command/command.go
@@ -65,8 +65,54 @@ func HandleCommand(text string, user *model.User, isUser bool) string {
 			//TODO TwitchEventSubID
 		}
 		return fmt.Sprintf("delete %v successful!", streamerName)
+	case "/subyt":
+		//step 0: regex match command
+		re := regexp.MustCompile("^(/subyt)\\s([a-zA-Z0-9_]{4,24}$)")
+		if matched := re.MatchString(text); !matched {
+			//TODO handle reply message
+			return "Wrong format of command"
+		}
+		args := re.FindStringSubmatch(text)
+		channelId := args[2]
+		fmt.Println(channelId)
+		//step 1: check if the YtChannel exist
+		//TODO
+
+		//step 2: check if already sub to youtube PubSub
+		//if !model.DB.CheckYtChannelExist(channelId) {
+		//	youtubemod.YC.CreatePubSubByChannelId(channelId)
+		//}
+		//step 3: write into DB
+		//model.DB.CreateYtSubscription(&model.YtSubscription{
+		//	Line:            user.Line,
+		//	LineAccessToken: user.LineAccessToken,
+		//	ChannelId:       channelId,
+		//})
+		//TODO: using channelName
+		return fmt.Sprintf("sub %v successful!", channelId)
+	case "/delyt":
+		//step 0: regex match command
+		re := regexp.MustCompile("^(/del)\\s([a-zA-Z0-9_]{4,24}$)")
+		if matched := re.MatchString(text); !matched {
+			//TODO handle reply message
+			return "Wrong format of command"
+		}
+		args := re.FindStringSubmatch(text)
+		channelId := args[2]
+		//step 1: delete DB record
+		//err := model.DB.DeleteSubByUserChannelId(user.Line, channelId)
+		//if err == model.ErrRecordNotExist {
+		//	return "Wrong ChannelId name or not sub yet "
+		//}
+		//step 2: check if should remove PubSub
+		//TODO: unsubscribe from PubSub
+
+		//TODO: using channelName
+		return fmt.Sprintf("delete %v successful!", channelId)
 	case "/list":
 		subs := model.DB.QuerySubByUser(user.Line)
+		//ytSubs := model.DB.QueryYtSubByUser(user.Line)
+		//if len(subs) == 0 && len(ytSubs) == 0 {
 		if len(subs) == 0 {
 			return "You haven't subscribe any channel!"
 		}
@@ -74,13 +120,18 @@ func HandleCommand(text string, user *model.User, isUser bool) string {
 		for _, sub := range subs {
 			resp += fmt.Sprintf("https://www.twitch.tv/%s\n", sub.TwitchLoginName)
 		}
+		//for _, sub := range ytSubs {
+		//	resp += fmt.Sprintf("https://www.youtube.com/channel/%s\n", sub.ChannelId)
+		//}
 		return resp
 	case "/help":
 		//TODO add  line emoji
 		return fmt.Sprint("指令清單:\n" +
 			"1. /sub [twitch ID]: 訂閱頻道\n   Example: /sub never_loses\n" +
 			"2. /del [twitch ID]: 刪除頻道\n   Example: /del qq7925168\n" +
-			"3. /list: 列出訂閱的頻道")
+			"3. /subyt [Youtube Channel ID]: 訂閱頻道\n   Example: /sub UC1DCedRgGHBdm81E1llLhOQ\n" +
+			"4. /delyt [Youtube Channel ID]: 刪除頻道\n   Example: /del UC1DCedRgGHBdm81E1llLhOQ\n" +
+			"5. /list: 列出訂閱的頻道")
 	}
 	return "No this command, please check /help"
 }
